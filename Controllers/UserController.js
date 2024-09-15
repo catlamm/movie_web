caa6cfe3383a67f85efe7eb956fbe855017c1800
catlamm <lamb2111988@student.ctu.edu.vn>
2024-09-16 00:33:23 +0700

    movie project

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -9,11 +9,11 @@ import { generateToken } from "../middlewares/Auth.js";
 
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-    const {fullName, email, password, image} = req.body;
+    const { fullName, email, password, image } = req.body;
     try {
-        const userExists = await User.findOne({email})
+        const userExists = await User.findOne({ email })
         // check if user exists
-        if(userExists) {
+        if (userExists) {
             res.status(400)
             throw new Error("User already exists")
         }
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async (req, res) => {
         });
 
         // if user created successfully send user data to client
-        if(user) {
+        if (user) {
             res.status(201).json({
                 _id: user._id,
                 fullName: user.fullName,
@@ -40,15 +40,80 @@ const registerUser = asyncHandler(async (req, res) => {
                 token: generateToken(user._id),
             });
         }
-        
-        else{
+
+        else {
             res.status(400);
             throw new Error("Invalid user data");
         }
 
     } catch (error) {
-        
+        res.status(400).json({ message: error.message });
     }
 });
 
-export { registerUser };
+
+// @desc Login user
+// @route POST /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email});
+        // if user exists compare password with hashed password then send user data token to client
+        if ( user && (await bcrypt.compare(password, user.password))){
+            res.json({
+                _id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                image: user.image,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id),
+            });
+            // if user is not found or password not match send error message       
+        }else{
+            res.status(401);
+            throw new Error("Invalid email or password");
+        }
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+});
+
+// ***********PRIVATE CONTROLLER *************
+
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const { fullName, email, image } = req.body;
+try {
+    //find user in DB
+    const user = await User.findById(req.user._id);
+    // if user exists update his profile and send updated user data to client
+    if (user) {
+        user.fullName = fullName || user.fullName;
+        user.email = email || user.email;
+        user.image = image || user.image;
+
+        const updatedUser = await user.save();
+        // send updated user data and token to client
+        res.json({
+            _id: updatedUser._id,
+            fullName: updatedUser.fullName,
+            email: updatedUser.email,
+            image: updatedUser.image,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id),
+        });
+    } 
+    // else send error message
+    else{
+        res.status(404);
+        throw new Error("User not found");
+    }
+}catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+export { registerUser, loginUser, updateUserProfile};
--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { registerUser } from '../Controllers/UserController.js';
+import { 
+    loginUser, 
+    registerUser, 
+    updateUserProfile,  
+} from '../Controllers/UserController.js';
+import { protect } from '../middlewares/Auth.js';
 
 const router = express.Router();
 
 // *********PUBLIC ROUTES******************
 router.post("/", registerUser);
+router.post('/login', loginUser);
+
+//*********PRIVATE ROUTES****************
+router.put("/", protect, updateUserProfile);
 
 export default router;
