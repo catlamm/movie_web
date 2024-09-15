29274fb536dc9e4a867a58dddd05942ec41ed784
catlamm <lamb2111988@student.ctu.edu.vn>
2024-09-15 17:41:55 +0700

    movie project

---
+++ Controllers/UserController.js
@@ -0,0 +1,54 @@
+import asyncHandler from "express-async-handler";
+import User from "../Models/UserModel.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../middlewares/Auth.js";
+
+// @desc Registers user
+// @route POST /api/users
+
+
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const {fullName, email, password, image} = req.body;
+    try {
+        const userExists = await User.findOne({email})
+        // check if user exists
+        if(userExists) {
+            res.status(400)
+            throw new Error("User already exists")
+        }
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // create user in DB
+        const user = await User.create({
+            fullName,
+            email,
+            password: hashedPassword,
+            image,
+        });
+
+        // if user created successfully send user data to client
+        if(user) {
+            res.status(201).json({
+                _id: user._id,
+                fullName: user.fullName,
+                email: user.email,
+                image: user.image,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id),
+            });
+        }
+        
+        else{
+            res.status(400);
+            throw new Error("Invalid user data");
+        }
+
+    } catch (error) {
+        
+    }
+});
+
+export { registerUser };
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import {connectDB} from "./config/db.js";
+import userRouter from "./Routes/UserRouter.js";
+import { errorHandler } from './middlewares/errorMiddleware.js';
 
 dotenv.config();
 
@@ -13,9 +15,16 @@ app.use(express.json());
 
 connectDB();
 
+
+// Main route
 app.get('/', (req, res) => {
     res.send('API is running...')
 })
+//other route
+app.use("/api/users", userRouter);
+
+// error handling middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
